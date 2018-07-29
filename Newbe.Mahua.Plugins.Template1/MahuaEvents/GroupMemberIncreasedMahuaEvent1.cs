0570885271580e8a6144f12d2a9beba0dcd2913b
明高 王 <0sdf0@sina.cn>
2018-07-29 23:20:04 +0800

    ，

---
+++ Newbe.Mahua.Plugins.Template1/MahuaEvents/GroupMemberIncreasedMahuaEvent1.cs
@@ -0,0 +1,28 @@
+﻿using Newbe.Mahua.MahuaEvents;
+using System;
+
+namespace Newbe.Mahua.Plugins.Template1.MahuaEvents
+{
+    /// <summary>
+    /// 群成员增多事件
+    /// </summary>
+    public class GroupMemberIncreasedMahuaEvent1
+        : IGroupMemberIncreasedMahuaEvent
+    {
+        private readonly IMahuaApi _mahuaApi;
+
+        public GroupMemberIncreasedMahuaEvent1(
+            IMahuaApi mahuaApi)
+        {
+            _mahuaApi = mahuaApi;
+        }
+
+        public void ProcessGroupMemberIncreased(GroupMemberIncreasedContext context)
+        {
+            // todo 填充处理逻辑
+            throw new NotImplementedException();
+
+            // 不要忘记在MahuaModule中注册
+        }
+    }
+}
